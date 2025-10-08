0fafffea3861c4f6fd5aaecc120c29a51914e9ae
USAMI Mamoru <usami@matelier-MBP.AirPort>
2025-10-08 11:58:46 +0900

    Aシステムは廃止されました。Hシステムを使って説明します。

--- FOCUS/phase0.sh
+++ FOCUS/phase0.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -p a024h
-#SBATCH -n 12
-#SBATCH -t 0:10:00
-#SBATCH -N 1
+#SBATCH -p h006m
+#SBATCH -t 0:06:00
+#SBATCH -n 16
+#SBATCH -N 2
 #SBATCH -J PHASE0
 #SBATCH -o stdout.%J.log
 #SBATCH -e stderr.%J.log
 
-source /home1/share/opt/intel-2023.0.0/setvars.sh
+source /home1/share/x86_64/archive/opt/intel-2023.0.0/setvars.sh
 
-PHASE=~/phase0_2024.01/bin/phase
+PHASE=~/phase0_2025/bin/phase
 
-mpirun -n 12 $PHASE # ne=1 nk=12
+mpirun -n 16 $PHASE # ne=1 nk=16
